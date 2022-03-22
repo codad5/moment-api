95437779d9f3dc20cc4459718c30aa0f56bd8b7c
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2022-03-22 10:06:27 +0100

    signup login api adjusted

---
+++ index.php
@@ -0,0 +1,9 @@
+
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Headers: X-Requested-With');
+header('Access-Control-Allow-Headers: ');
+header('Access-Control-Allow-Headers: x-rapidapi-key, x-rapidapi-host');
+header('Access-Control-Allow-Headers: content-type');
+header('Content-Type: application/json');
